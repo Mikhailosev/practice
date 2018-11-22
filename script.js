a5991ecb952c17102f4f6bd24c2bfb2b251194ae
Mikhailosev <mikhail.losev2@gmail.com>
2018-11-22 03:48:05 +0300

    New way

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
 $(document).ready(function() {
-    let long;
-    let lat;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-
             let long = position.coords.longitude;
             let lat = position.coords.latitude;
-
-            const api = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+config.api;
-
+            const api = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid='+config.api;
             $.getJSON(api, function(data) {
-                let fTemp;
-                let cTemp;
                 let kTemp;
                 let tempSwap=true;
                 let weatherType=data.weather[0].description
                 kTemp=data.main.temp;
                 let windSpeed=data.wind.speed;
                 let city=data.name;
-                fTemp=(kTemp*(9/5)-459.67).toFixed(1);
-                cTemp=(kTemp-273).toFixed(1);
             console.log(city);
             $('#city').html(city);
             $('#weatherType').html(weatherType);
-            $('#fTemp').html(fTemp+" &deg;F");
+            $('#fTemp').html(fTemp(kTemp)+" &deg;F");
             $('#fTemp').click(function(){
                 if(tempSwap===false){
-                    $('#fTemp').html(cTemp+" &deg;C");
+                    $('#fTemp').html(cTemp(kTemp)+" &deg;C");
                     tempSwap=true;
                 }
                 else{
-                $('#fTemp').html(fTemp+" &deg;F");
+                $('#fTemp').html(fTemp(kTemp)+" &deg;F");
                 tempSwap=false;
                 }
             });
@@ -39,3 +30,5 @@ $(document).ready(function() {
         });
     }
 });
+const fTemp=(temp)=>{document.getElementById('fTemp').textContent=console.log((temp*(9/5)-459.67).toFixed(1))}
+const cTemp=(temps)=>{document.getElementById('fTemp').textContent=console.log((temps-273).toFixed(1))}
